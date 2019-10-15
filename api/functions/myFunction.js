39b89e90f94d4f64e8dbbfb626760fa458d711c7
Severi Haverila <severi.haverila@gmail.com>
2019-10-15 14:47:52 +0300

    integrate with frontend

--- api/functions/myFunction.js
+++ api/functions/myFunction.js
@@ -2,6 +2,9 @@
 export const main = async (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+    },
     body: JSON.stringify({
       hello: "world",
     })
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,21 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const onClick = async () => {
+    const response = await fetch(`${process.env.REACT_APP_ServiceEndpoint}/myfunction`);
+    const data = await response.json();
+    console.log(data);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={onClick}>API call</button>
       </header>
     </div>
   );
